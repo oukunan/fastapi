868d16c0ca04c8db66d66491a09f32c0425cb72b
futurouz <kunanan.tw@gmail.com>
2024-11-22 11:48:40 +0700

    chore: update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 
 app.include_router(messages.router)
 app.include_router(channels.router)
+
+
+@app.get('/')
+def read_root():
+    return { "message": "Welcome, kindly visit /docs"}
